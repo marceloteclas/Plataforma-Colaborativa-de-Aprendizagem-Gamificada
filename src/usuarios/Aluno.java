f5b750156d9a33dd809ad3dbb838ca052e3e7def
FranklinFelixADS <20201TADSSAJ0009@ifba.edu.br>
2025-09-25 15:17:01 -0300

    Add registro de conquistas e medalhas ao Aluno

--- src/usuarios/Aluno.java
+++ src/usuarios/Aluno.java
@@ -1,6 +1,12 @@
 package usuarios;
 
+import conquistas_reestruturadas.ConquistaComposite;
+import conquistas_reestruturadas.MedalhaComposite;
+
 public class Aluno extends Usuarios {
+    private ConquistaComposite minhasConquistas = new ConquistaComposite("Minhas Conquistas", "Conquistas desbloqueadas pelo aluno");
+    private MedalhaComposite minhasMedalhas = new MedalhaComposite("Minhas Medalhas", "Medalhas recebidas pelo aluno");
+    
     public Aluno(String nome, String senha) {
         super(nome, senha);
     }
@@ -10,4 +16,19 @@ public class Aluno extends Usuarios {
         return "Aluno";
     }
 
+    public ConquistaComposite getMinhasConquistas() {
+        return minhasConquistas;
+    }
+
+    public MedalhaComposite getMinhasMedalhas() {
+        return minhasMedalhas;
+    }
+
+    public void adicionarConquista(ConquistaComposite conquista) {
+        this.minhasConquistas.adicionar(conquista);
+    }
+
+    public void adicionarMedalha(MedalhaComposite medalha) {
+        this.minhasMedalhas.adicionar(medalha);
+    }
 }
